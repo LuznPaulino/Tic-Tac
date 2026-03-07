e7751d7d3d4f14ba6ed2033bc89c9c11d70b5ff1
Luz Paulino <62310498+LuznPaulino@users.noreply.github.com>
2026-03-07 15:34:29 -0500

    Script Updates
    Added a ton of inputs into the main script:
    
    Player input variety (Player1 and Player2), board detection, cell size, game reset, grid array for memory.

--- simple-hard/Scripts/main.gd
+++ simple-hard/Scripts/main.gd
@@ -1,20 +1,51 @@
 extends Node
-var board_size: int
-# Called when the node enters the scene tree for the first time.
+
+# BRINGS IN DIFFERENT SCENES INSTEAD OF LINKING
+@export var circleScene: PackedScene
+@export var crossScene: PackedScene
+
+
+var player: int # PLAYER(S) CLICKS
+var gridDate: Array # GRID
+var gridPosition: Vector2i # POSITIONS
+var boardSize: int # BOARD
+var cellSize: int # CELLS
+
 func _ready() -> void:
-	#When the game starts
-	board_size = $Board.texture.get_width()
-	print(board_size)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+	boardSize = $Board.texture.get_width()
+	cellSize = boardSize/3
+	newGame()
+	
 func _process(delta: float) -> void:
 	pass
-	
+
+# CHECKS THE INPUT OF CLICKS + GRIDLOCK POSITIONS
 func _input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-			if event.position.x < board_size:
-				print(event.position)
-			
-			
+			if event.position.x < boardSize:
+				gridPosition = Vector2i(event.position / cellSize)
+				if gridDate[gridPosition.y][gridPosition.x] == 0:
+					gridDate[gridPosition.y][gridPosition.x] = player
+					createMarker(player, gridPosition * cellSize + Vector2i(cellSize/2, cellSize/2))
+					player *= -1
+					print(gridDate) 
+
+#STARTS THE GAME BOARD ARRAY AT 0
+func newGame():
+	player = 1
+	gridDate = [[0,0,0],
+				[0,0,0],
+				[0,0,0]
+				]
+
+# MAKES THE X AND 0 APPEAR ON THE BOARD
+func createMarker(player, position):
+	if player == 1:
+		var circle = circleScene.instantiate()
+		circle.position = position
+		add_child(circle)
+	if player == -1:
+		var cross = crossScene.instantiate()
+		cross.position = position
+		add_child(cross)
