d67404b15073c947e170078cc9db4f8db947cecb
Luz Paulino <62310498+LuznPaulino@users.noreply.github.com>
2026-03-07 16:31:18 -0500

    Script + Scene Updates
    Added a side panel, along with a same display to indicate what player is next on the board

--- simple-hard/Scripts/main.gd
+++ simple-hard/Scripts/main.gd
@@ -6,6 +6,8 @@ extends Node
 
 
 var player: int # PLAYER(S) CLICKS
+var tempMarker
+var panPostion: Vector2i # DISPLAY NEXT MOVE
 var gridDate: Array # GRID
 var gridPosition: Vector2i # POSITIONS
 var boardSize: int # BOARD
@@ -14,6 +16,7 @@ var cellSize: int # CELLS
 func _ready() -> void:
 	boardSize = $Board.texture.get_width()
 	cellSize = boardSize/3
+	panPostion = $PlayerPanel.get_position()
 	newGame()
 	
 func _process(delta: float) -> void:
@@ -27,8 +30,12 @@ func _input(event):
 				gridPosition = Vector2i(event.position / cellSize)
 				if gridDate[gridPosition.y][gridPosition.x] == 0:
 					gridDate[gridPosition.y][gridPosition.x] = player
+					# DISPLAY MARKER STARTING POINT
 					createMarker(player, gridPosition * cellSize + Vector2i(cellSize/2, cellSize/2))
 					player *= -1
+					# UPDATING DISPLAY MAKRER
+					tempMarker.queue_free()
+					createMarker(player, panPostion + Vector2i(cellSize/2, cellSize/2), true)
 					print(gridDate) 
 
 #STARTS THE GAME BOARD ARRAY AT 0
@@ -38,14 +45,18 @@ func newGame():
 				[0,0,0],
 				[0,0,0]
 				]
+	# CREATING MARKER TO SHOW WHO'S TURN IT IS
+	createMarker(player, panPostion + Vector2i(cellSize/2, cellSize/2), true)
 
 # MAKES THE X AND 0 APPEAR ON THE BOARD
-func createMarker(player, position):
+func createMarker(player, position, temp=false):
 	if player == 1:
 		var circle = circleScene.instantiate()
 		circle.position = position
 		add_child(circle)
-	if player == -1:
+		if temp: tempMarker = circle
+	else:
 		var cross = crossScene.instantiate()
 		cross.position = position
 		add_child(cross)
+		if temp: tempMarker = cross
